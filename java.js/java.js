d2f344cff198b07f5d4f9703ba97b7ee96772240
karlasantic <221443@buas.nl>
2024-02-29 19:19:46 +0100

    Name animattion and link colour

---
+++ java.js/java.js
@@ -0,0 +1,15 @@
+// Assuming you are using the Typed.js library for the typing animation
+var options = {
+    strings: ['Karla Šantić'],
+    typeSpeed: 50, // typing speed in milliseconds
+    loop: true // loop the animation infinitely
+  }
+  
+  var typed = new Typed('.typed', options);
+  var options = {
+    strings: ['Karla Šantić'],
+    typeSpeed: 50, // typing speed in milliseconds
+    loop: true // loop the animation infinitely
+  }
+  
+  var typed = new Typed('#typingAnimation', options);
